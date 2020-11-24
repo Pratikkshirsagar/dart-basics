ad3a4088f7b1c360d69600582f7154f33eaa9cc5
Pratikkshirsagar <meetpratiik1995@gmail.com>
2020-11-25 00:24:25 +0530

    interpolation example added

--- string-concatenation.dart
+++ string-concatenation.dart
@@ -7,4 +7,8 @@ void main() {
   print(
       "My name is $firstName $LastName and I'm $age years old, I'm $height meter tall.");
   print("Next year I will be ${age + 1} yeats old.");
+
+  //  string interpolation
+  double temp = 30;
+  print("${temp}C");
 }
