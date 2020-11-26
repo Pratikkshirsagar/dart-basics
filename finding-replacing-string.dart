7da780bc6cff30523a3bcef45c7fce0e55dd8417
Pratikkshirsagar <meetpratiik1995@gmail.com>
2020-11-26 17:20:39 +0530

    string find and replace example

---
+++ finding-replacing-string.dart
@@ -0,0 +1,9 @@
+void main() {
+  // finding a value
+  String lovePizza = 'I love pizza';
+  print(lovePizza.contains('pizza'));
+
+  // replacing a value
+  String lovePasta = lovePizza.replaceAll('pizza', 'pasta');
+  print(lovePasta);
+}
