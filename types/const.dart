78a5cf0433295a0d1761d65fbe56812b1a96515c
Pratikkshirsagar <meetpratiik1995@gmail.com>
2020-11-30 00:46:26 +0530

    const example

---
+++ types/const.dart
@@ -0,0 +1,10 @@
+void main() {
+  const x = 1;
+  const y = 2;
+  const z = x + y;
+
+  // var can be set more than once
+  // final can be set only once
+  // const compile-time constance
+  // const > final > var
+}
