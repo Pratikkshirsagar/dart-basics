782d5f28d5b1ebbd303e2a6f54f4ec66bf7cc6db
Pratikkshirsagar <meetpratiik1995@gmail.com>
2020-11-30 23:45:18 +0530

    enum example

---
+++ control-flow/enumeration.dart
@@ -0,0 +1,19 @@
+enum Medal { gold, silver, bronze, noMedal }
+
+void main() {
+  const medal = Medal.gold;
+
+  switch (medal) {
+    case Medal.gold:
+      print('gold');
+      break;
+    case Medal.silver:
+      print('silver');
+      break;
+    case Medal.bronze:
+      print('bronze');
+      break;
+    case Medal.noMedal:
+      print('no medal, try again');
+  }
+}
