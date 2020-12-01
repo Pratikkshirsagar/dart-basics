38dda89e61f5041929f9573d2b85df89ac05b322
Pratikkshirsagar <meetpratiik1995@gmail.com>
2020-12-01 23:59:44 +0530

    as operator example

--- map/map.dart
+++ map/map.dart
@@ -1,6 +1,6 @@
 void main() {
   var person = <String, dynamic>{'name': 'Pratik', 'age': 25, 'height': 1.82};
-  var name = person['name']; // access the value
+  var name = person['name'] as String; // access the value
   person['age'] = 27;
   person['likePizza'] = true;
   print(name);
