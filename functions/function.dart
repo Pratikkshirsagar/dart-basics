640196bca1cf6ef761714d080274fa0804943bb1
Pratikkshirsagar <meetpratiik1995@gmail.com>
2020-12-02 22:05:37 +0530

    function example

---
+++ functions/function.dart
@@ -0,0 +1,12 @@
+void main() {
+  helloWorld();
+  describe('Pratik', 25);
+}
+
+void helloWorld() {
+  print('Hello World');
+}
+
+void describe(String name, int age) {
+  print("My name is $name and I'am $age years old");
+}
