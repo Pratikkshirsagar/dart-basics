02b583492ee9ddc50eea0069c3748496e21c53a0
Pratikkshirsagar <meetpratiik1995@gmail.com>
2020-12-09 00:23:25 +0530

    static example

---
+++ classes/static.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+class Strings {
+  static String welcome = 'Welcome';
+  static String signIn = 'Sign In';
+}
+
+Void main() {
+  print(Strings.welcome);
+  print(Strings.signIn);
+}
