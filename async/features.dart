0e2952771f50033e7464bfe923ed38559294bed1
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-01-18 23:38:12 +0530

    future value and error implemented

--- async/features.dart
+++ async/features.dart
@@ -1,6 +1,15 @@
 Future<String> fetchUserOrder() =>
     Future.delayed(Duration(seconds: 2), () => 'Cappuccino');
 
+
+Future<String> fetchUserOrder2() => Future.value('Espresso');
+
+Future<String> fetchUserOrder3() => Future.error(Exception('Out of milk'));
+
+
+Future<String> fetchUserOrder3() => Future.error(UnimplementedError());
+
+
 void main() {
   fetchUserOrder()
       .then((order) => print('Order is ready: $order'))
